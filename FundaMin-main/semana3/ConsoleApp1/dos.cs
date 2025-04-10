5dd604b64f02f4b33613c6ba842aef25a0d04fbd
josgtUPN <N00478047@upn.pe>
2025-04-09 19:09:11 -0500

    Add files via upload

---
+++ FundaMin-main/semana3/ConsoleApp1/dos.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Dos
+    { static void Main(string[] args)
+            {
+            double n1, n2, n3;
+            Console.WriteLine("ingrese el primer numero: ");
+            n1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese el segundo numero: ");
+            n2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese tercer numero: ");
+            n3 = double.Parse(Console.ReadLine());
+
+            if ((n1 > n2 && n1 < n3) || (n1 < n2 && n1 > n3))
+            {
+                Console.WriteLine("El numero del medio es: " + n1);
+
+            }
+            else if ((n2 < n1 && n2 < n3) || (n2 < n1 && n2 > n3))
+            {
+                Console.WriteLine("el numero del medio es: " + n2);
+            }
+            else
+                Console.WriteLine("el numero del medio es: " + n3);
+            Console.ReadKey();  
+            }
+    }
+}
