929e2a9573555b4b60da051f3d4c757d771734df
GTJosUPN <N00478047@upn.pe>
2025-05-21 21:41:14 -0500

    21:40 proceso de expo del viernes tarea semana 8

---
+++ FundaDua-V/Tareas/semana8_ejercicio6.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tareas
+{
+    class semana8_ejercicio6
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ingrese su nombre: ");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("ingresa tu apellido: ");
+            string apellido = Console.ReadLine();
+            saludar(nombre, apellido);
+            Console.ReadLine();
+
+        }
+        public static void saludar(string nombre, string apellido)
+        { 
+            Console.WriteLine("hola {0}___{1}.", nombre, apellido);
+            Console.WriteLine("¡hola" + nombre + "!");
+        }
+    }
+}
